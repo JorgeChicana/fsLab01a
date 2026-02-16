2a829d934fb387c37508b358425dda131e5d37d7
JorgeChicana <desarrollochicana@gmail.com>
2026-02-16 17:30:21 -0500

    Agregar carpeta backend con Express

---
+++ backend01/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+app.get('/api', (req, res) => {
+  res.send({ mensaje: "Hola desde Express (Org.Jorge Chicana)" });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Servidor corriendo ${PORT}`));
