aedf85849096ab344e9de961e520bb933e0d48cd
JorgeChicana <desarrollochicana@gmail.com>
2026-02-16 11:42:24 -0500

    Agregar el proyecto inicial

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>React con Vite listo</h1>
+      <h3>Org. Jorge Chicana</h3>      
+      <p>Backend con Express corriendo en http://localhost:5137</p>
+    </div>
+  );
+}
+export default App;
